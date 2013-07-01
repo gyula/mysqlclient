bc4f223616a3bd1d9ec16f2eab6d2f67835886e0
Gyula Petrovics <gyulusa@gmail.com>
2013-07-01 10:42:24 +0200

    test

--- main.c
+++ main.c
@@ -4,5 +4,19 @@
 
 int main(int argc, char **argv)
 {
+	MYSQL_RES *result;
+	MYSQL_ROW row;
+	MYSQL *connection, mysql;
+	int state;
+
+	//print out the installed MySQL client version	
 	printf("MySQL client version: %s\n", mysql_get_client_info());
+	
+	//initialize the connection
+	mysql_init(&mysql);
+	//set connection details
+	connection = (&mysql,"localhost","root","topsecret","employees",0,0,0);
+	  if(connection == NULL);
+	   printf(mysql_error(&mysql));
+	return -1;
 }
