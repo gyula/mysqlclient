4775d57a8c47c3aff15121be191f06b38267ad42
Gyula Petrovics <gyulusa@gmail.com>
2013-07-01 23:58:21 +0200

    test

--- main.c
+++ main.c
@@ -4,23 +4,24 @@
 
 int main(int argc, char **argv)
 {
-	MYSQL_RES *result;
-	MYSQL_ROW row;
-	MYSQL *connection, mysql;
-	int state;
+ MYSQL_RES *result;
+ MYSQL_ROW row;
+ MYSQL *connection, mysql;
+ int state;
 
-	//print out the installed MySQL client version	
-	printf("MySQL client version: %s\n", mysql_get_client_info());
-	
-	//initialize the connection
-	mysql_init(&mysql);
-	//set connection details
-	connection = (&mysql,"localhost","root","topsecret","employees",0,0,0);
-	  if(connection == NULL)
-	  {
-	   printf(mysql_error(&mysql));
-	   return -1;
-	  }
-	
-	mysql_close(connection);
+//print out the installed MySQL client version
+ printf("MySQL client version: %s\n", mysql_get_client_info());
+
+//initialize the connection
+ mysql_init(&mysql);
+//set connection details
+ connection = (&mysql,"localhost","root","topsecret","employees",0,0,0);
+  if(connection == NULL)
+  {
+   printf(mysql_error(&mysql));
+   return 1;
+  }
+
+//close connection
+ mysql_close(connection);
 }
