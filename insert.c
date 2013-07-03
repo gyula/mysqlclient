ed5a7cacd5b26a72b8e027718d6333073c808a78
Gyula Petrovics <gyulusa@gmail.com>
2013-07-03 18:12:50 +0200

    insert statement in loop

--- insert.c
+++ insert.c
@@ -5,7 +5,7 @@
 int main()
 {
   MYSQL *connection, mysql;
-
+  int i;
   //set the connection
   mysql_init(&mysql);
   connection = mysql_real_connect(&mysql, "localhost", "root", "topsecret", "test" ,0,0,0);
@@ -14,11 +14,20 @@ int main()
     printf(mysql_error(&mysql));
     return 1;
   }
+  //create table if not exsists
   if(mysql_query(connection, "CREATE TABLE IF NOT EXISTS test.Testinsert(ID INT, MSG CHAR(15));"))
   {
     printf(mysql_error(connection));
     return 1;
   }
-  //close the connection
-  mysql_close(connection);
+
+  char statement[100];
+  for(i=0; i < 100; i++)
+  {
+      snprintf(statement,100,"INSERT INTO Testinsert VALUES('%d','log_msg');",i);
+  //printf("%s",statement);
+      mysql_query(connection, statement);
+  } 
+ //close the connection
+      mysql_close(connection);
 }
