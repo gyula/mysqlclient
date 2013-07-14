4722fb36382d4b2884a930ad3426853acab9878f
Gyula Petrovics <gyulusa@gmail.com>
2013-07-14 10:46:07 +0200

    performance optimisation

--- load.c
+++ load.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <time.h>
 #include <mysql.h>
+#include <stdlib.h>
 
 long double time_elapsed_msec(struct timeval begin_time, struct timeval end_time)
 {
   return ((end_time.tv_sec - begin_time.tv_sec)*1000000 + (end_time.tv_usec - begin_time.tv_usec))/1000;
 }
 
+void error_msg(MYSQL *connection)
+{
+  printf("%s\n",mysql_error(connection));
+  exit(1);
+}
 int main()
 {
   MYSQL *connection, mysql;
@@ -26,11 +32,44 @@ int main()
     printf(mysql_error(connection));
     return 1;
   }
-
+  if(mysql_query(connection, "SET FOREIGN_KEY_CHECKS = 0;"))
+  {
+     printf(mysql_error(&mysql));
+     return 1;
+  }
+  if(mysql_query(connection, "SET UNIQUE_CHECKS = 0;"))
+  {
+    error_msg(connection);
+  }
+  if(mysql_query(connection, "SET SESSION tx_isolation = 'READ-UNCOMMITTED'"))
+  {
+    error_msg(connection);
+  }
+  if(mysql_query(connection, "SET sql_log_bin = 0;"))
+  {
+    error_msg(connection);
+  }
   char statement[]= "LOAD DATA INFILE '/home/plagueis/Projects/log' INTO TABLE Testload fields terminated by ' ' lines terminated by '\n'";
   gettimeofday(&begin,0);
   mysql_query(connection, statement);
   gettimeofday(&end,0);
+  if(mysql_query(connection, "SET FOREIGN_KEY_CHECKS = 1;"))
+  {
+     printf(mysql_error(&mysql));
+     return 1;
+  }
+  if(mysql_query(connection, "SET UNIQUE_CHECKS = 1;"))
+  {
+     error_msg(connection);
+  }
+  if(mysql_query(connection, "SET SESSION tx_isolation='READ-UNCOMMITTED'"))
+  {
+     error_msg(connection);
+  }
+  if(mysql_query(connection, "SET sql_log_bin = 1"))
+  {
+    error_msg(connection);
+  }
   printf("Qeury time:  %.3Lf msec\n", time_elapsed_msec(begin,end));
  //close the connection
   mysql_close(connection);
