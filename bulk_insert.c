0216d37a9879a0d265bac39855edefb2fc5e0457
Gyula Petrovics <gyulusa@gmail.com>
2013-07-15 22:29:09 +0200

    error_msg function

--- bulk_insert.c
+++ bulk_insert.c
@@ -2,10 +2,17 @@
 #include <time.h>
 #include <mysql.h>
 #include <glib.h>
+#include <stdlib.h>
 
 long double time_elapsed_msec(struct timeval begin_time, struct timeval end_time)
 {
   return ((end_time.tv_sec - begin_time.tv_sec)*1000000 + (end_time.tv_usec - begin_time.tv_usec))/1000;
+}
+
+void error_msg(MYSQL *connection)
+{
+  printf("%s\n",mysql_error(connection));
+  exit(1);
 }
 
 int main()
@@ -23,20 +30,16 @@ int main()
   connection = mysql_real_connect(&mysql, "localhost", "root", "topsecret", "test" ,0,0,0);
   if(connection == NULL)
   {
-    printf(mysql_error(&mysql));
-    return 1;
+    error_msg(connection);
   }
   //create table if not exsists
   if(mysql_query(connection, "CREATE TABLE IF NOT EXISTS test.Testinsert(ID INT, MSG CHAR(15)) ENGINE=InnoDB;"))
   {
-    printf(mysql_error(connection));
-    return 1;
+    error_msg(connection);
   }
-
   if(mysql_query(connection, "SET autocommit=0;"))
   {
-    printf(mysql_error(connection));
-    return 1;
+    error_msg(connection);
   }
   gettimeofday(&begin,0);
   for( i = 0  ; i < max; i++)
@@ -50,13 +53,11 @@ int main()
   g_string_append(statement, ";");
   if(mysql_query(connection, statement -> str))
     {
-      printf(mysql_error(connection));
-      return 1;
+      error_msg(connection);
     }
   if(mysql_query(connection, "COMMIT;"))
     {
-      printf(mysql_error(connection));
-      return 1;
+      error_msg(connection);
     }
   gettimeofday(&end,0);
   printf("Query time:  %.3Lf msec\n", time_elapsed_msec(begin,end));
